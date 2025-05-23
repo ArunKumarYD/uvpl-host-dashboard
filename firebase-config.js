1842f1c112fe9894da35d5d10e0f24cac035ff0f
ArunKumarYD <arun.k@alpinecreations.com>
2025-05-23 15:20:41 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
-// Paste your Firebase config here
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project.firebaseio.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id"
+  apiKey: "AIzaSyDpw0FrC5w71BAclOZ_RSt4oA2qOoRXp30",
+  authDomain: "uvpl-86a36.firebaseapp.com",
+  databaseURL: "https://uvpl-86a36-default-rtdb.firebaseio.com",
+  projectId: "uvpl-86a36",
+  storageBucket: "uvpl-86a36.appspot.com",
+  messagingSenderId: "518611214976",
+  appId: "1:518611214976:web:239608f4e5aad7a1d97c65",
+  measurementId: "G-WQ692S92EP"
 };
 firebase.initializeApp(firebaseConfig);
+
 const firebaseConfig = {
   apiKey: "AIzaSyDpw0FrC5w71BAclOZ_RSt4oA2qOoRXp30",
   authDomain: "uvpl-86a36.firebaseapp.com",
