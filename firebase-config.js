1eefb8831c30742e1b0bdb9e70082a9af6c64624
ArunKumarYD <arun.k@alpinecreations.com>
2025-05-23 19:30:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,16 +9,3 @@ const firebaseConfig = {
   measurementId: "G-WQ692S92EP"
 };
 firebase.initializeApp(firebaseConfig);
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDpw0FrC5w71BAclOZ_RSt4oA2qOoRXp30",
-  authDomain: "uvpl-86a36.firebaseapp.com",
-  databaseURL: "https://uvpl-86a36-default-rtdb.firebaseio.com",
-  projectId: "uvpl-86a36",
-  storageBucket: "uvpl-86a36.appspot.com",
-  messagingSenderId: "518611214976",
-  appId: "1:518611214976:web:239608f4e5aad7a1d97c65",
-  measurementId: "G-WQ692S92EP"
-};
-
-firebase.initializeApp(firebaseConfig);
