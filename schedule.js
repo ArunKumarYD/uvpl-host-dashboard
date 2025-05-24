98b1b2963bcc207e3d452803f91e6e0c68f1df13
ArunKumarYD <arun.k@alpinecreations.com>
2025-05-24 16:10:37 +0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -2,20 +2,20 @@ const db = firebase.database();
 const scheduleTableBody = document.getElementById("scheduleTableBody");
 
 function loadSchedule() {
-  db.ref("schedule").on("value", snapshot => {
+  db.ref().on("value", snapshot => {
     scheduleTableBody.innerHTML = "";
     snapshot.forEach(child => {
       const match = child.val();
-      const row = document.createElement("tr");
-
-      row.innerHTML = `
-        <td>${match.date}</td>
-        <td>${match.time}</td>
-        <td>${match.teamA}</td>
-        <td>${match.teamB}</td>
-      `;
-
-      scheduleTableBody.appendChild(row);
+      if(match.date && match.time && match.teamA && match.teamB){
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${match.date}</td>
+          <td>${match.time}</td>
+          <td>${match.teamA}</td>
+          <td>${match.teamB}</td>
+        `;
+        scheduleTableBody.appendChild(row);
+      }
     });
   });
 }
