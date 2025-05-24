5019b7419ebed0a56751f996dafa90e5a2b5f574
ArunKumarYD <arun.k@alpinecreations.com>
2025-05-24 16:01:26 +0300

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ const db = firebase.database();
 const scheduleTableBody = document.getElementById("scheduleTableBody");
 
 function loadSchedule() {
-  db.ref("schedule").on("value", snapshot => { ... });
+  db.ref("schedule").on("value", snapshot => {
     scheduleTableBody.innerHTML = "";
     snapshot.forEach(child => {
       const match = child.val();
